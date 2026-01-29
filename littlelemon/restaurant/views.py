895999e83d0f97a31a434b51917259b399eae285
gladiola-lol <provisiones.00@outlook.com>
2026-01-29 14:14:48 -0800

    se agrego una api para las reservas

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,27 @@
+from rest_framework.decorators import api_view
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Menu
+from .models import Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuView(generics.RetrieveAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
